f20cc40dbbbe88f077baaa49ef02fec8e13e8f23
John Keen <johnkeen1805@gmail.com>
2017-12-04 16:38:18 +0000

    Wrapper js to load module from cdn

---
+++ tampermonkey.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         JIRA
+// @namespace    http://tampermonkey.net/
+// @version      0.7
+// @description  Make JIRA great again...(if it was ever.)
+// @author       Pete Arden
+// @match        https://yourdomain.atlassian.net/secure/RapidBoard.jspa*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    var s = document.createElement("script");
+    s.type = 'text/javascript';
+    s.src = 'https://cdn.rawgit.com/pda87/JIRABoard-TamperMonkey/master/jiraModule.js';
+    document.body.appendChild(s);
+})();
